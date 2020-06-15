b697cf46db74e4724b68f7a27b54130b874a718b
Stephen Rothwell <sfr@canb.auug.org.au>
2020-06-15 18:02:37 +1000

    xtensa: remove the include of major.h from files that do not need it
    This is fairly safe because major.h does not include any other files.
    The only real risk is if a symbol defined in major.h is constructed by
    concatenation in the preprocessor.
    
    The actual change was done using the following command:
    
      sed -i -E '/[<"](uapi\/)?linux\/major\.h/d' \
        $(grep -L -w -f xx \
          $(git grep -E -l '[<"](uapi/)?linux/major\.h' arch/xtensa))
    
    Where the file xx contians all the symbols defined in major.h
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/xtensa/platforms/xtfpga/setup.c
+++ arch/xtensa/platforms/xtfpga/setup.c
@@ -18,7 +18,6 @@
 #include <linux/reboot.h>
 #include <linux/kdev_t.h>
 #include <linux/types.h>
-#include <linux/major.h>
 #include <linux/console.h>
 #include <linux/delay.h>
 #include <linux/of.h>
