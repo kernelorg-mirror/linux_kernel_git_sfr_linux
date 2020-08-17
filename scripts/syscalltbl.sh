8b129e781e04ed6de78152afb1275610347f0112
Stephen Rothwell <sfr@canb.auug.org.au>
2020-08-18 07:03:57 +1000

    syscalls: concolidate the syscalltbl.sh scripts
    The only differences were the compat handling and the number
    of arguments to the __SYSCALL macro.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- scripts/syscalltbl.sh
+++ scripts/syscalltbl.sh
@@ -4,8 +4,19 @@
 in="$1"
 out="$2"
 my_abis=`echo "($3)" | tr ',' '|'`
-my_abi="$4"
-offset="$5"
+offset="$4"
+is_compat="$5"
+num_syscall_args="$6"
+ni_syscall="$7"
+
+if [ "$num_syscall_args" = "3" ]; then
+	syscall_fmt='__SYSCALL(%s, %s, )\n'
+else
+	syscall_fmt='__SYSCALL(%s,%s)\n'
+fi
+if [ -z "$ni_syscall" ];then
+	ni_syscall='sys_ni_syscall'
+fi
 
 emit() {
 	t_nxt="$1"
@@ -13,10 +24,10 @@ emit() {
 	t_entry="$3"
 
 	while [ $t_nxt -lt $t_nr ]; do
-		printf "__SYSCALL(%s,sys_ni_syscall)\n" "${t_nxt}"
+		printf "$syscall_fmt" "${t_nxt}" "$ni_syscall"
 		t_nxt=$((t_nxt+1))
 	done
-	printf "__SYSCALL(%s,%s)\n" "${t_nxt}" "${t_entry}"
+	printf "$syscall_fmt" "${t_nxt}" "${t_entry}"
 }
 
 grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
@@ -26,10 +37,10 @@ grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
 	fi
 
 	while read nr abi name entry compat ; do
-		if [ "$my_abi" = "64_o32" ] && [ ! -z "$compat" ]; then
+		if [ -n "$is_compat" ] && [ ! -z "$compat" ]; then
 			emit $((nxt+offset)) $((nr+offset)) $compat
 		else
-			emit $((nxt+offset)) $((nr+offset)) $entry
+		emit $((nxt+offset)) $((nr+offset)) $entry
 		fi
 		nxt=$((nr+1))
 	done
